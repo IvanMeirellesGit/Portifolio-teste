c5cf96a70cfc75f0148b7ecab61ddbabf85d56f3
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-10-20 11:57:29 -0300

    lightmode

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -2,14 +2,22 @@ let btnMenu = document.querySelector("#btn-menu");
 let menu = document.querySelector("#menu-mobile");
 let overlay = document.querySelector(".overlay-menu");
 
-btnMenu.addEventListener('click', ()=>{
+function abrirMenu(){
     menu.classList.add('abrir-menu')
+}
+function fecharMenu(){
+    menu.classList.remove('abrir-menu')
+}
+
+
+btnMenu.addEventListener('click', ()=>{
+    abrirMenu();
 })
 
 menu.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
+    fecharMenu();
 })
 
 overlay.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
+    fecharMenu();
 })
